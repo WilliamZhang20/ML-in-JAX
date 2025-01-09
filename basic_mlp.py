0798a92535902814eada99f5e2093980ee921b5f
Bill <wzhang20@yahoo.com>
2025-01-09 14:31:21 -0500

    remove redundant

--- basic_mlp.py
+++ basic_mlp.py
@@ -74,29 +74,7 @@ import torchvision.transforms as transforms
 
 def numpy_collate(batch):
     return tree_map(np.asarray, data)
-""""
-class NumpyLoader(data.DataLoader):
-    def __init__(self, dataset, batch_size=1, 
-                 shuffle=False, sampler=None,
-                 batch_sampler=None, num_workers=0,
-                 pin_memory=False, drop_last=False,
-                 timeout=0, worker_init_fn=None):
-        super(self.__class__, self).__init__(dataset, 
-            batch_size=batch_size,
-            shuffle=shuffle,
-            sampler=sampler,
-            batch_sampler=batch_sampler,
-            num_workers=num_workers,
-            collate_fn=numpy_collate,
-            pin_memory=pin_memory,
-            drop_last=drop_last,
-            timeout=timeout,
-            worker_init_fn=worker_init_fn)
 
-class FlattenAndCast(object):
-    def __call__(self, pic):
-        return np.ravel(np.array(pic, dtype=jnp.float32))
-"""
 transform = transforms.Compose([transforms.Lambda(lambda x: np.ravel(np.array(x, dtype=np.float32)))])
 
 # Load MNIST dataset with transformation
