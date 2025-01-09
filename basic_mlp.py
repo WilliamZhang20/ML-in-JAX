192dff068ebba082315cd2ec307a5b5e9e9a68f8
Bill <wzhang20@yahoo.com>
2025-01-08 22:54:47 -0500

    initial commit

---
+++ basic_mlp.py
@@ -0,0 +1,38 @@
+import numpy as np
+from jax import grad, jlt
+from jax import vmap
+from jax import random
+import jax
+import jax.numpy as jnp
+
+# random weights & biases
+def random_layer_params(m, n, key, scale=1e-2):
+    w_key, b_key = random.split(key)
+    return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n, ))
+
+# initialize layers
+def init_network_params(sizes, key):
+    keys = random.split(key, len(sizes))
+    return [random_layer_params(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+
+layer_sizes = [784, 512, 512, 10]
+step_size = 0.01
+num_epochs = 10
+batch_size = 128
+n_targets = 10
+params = init_network_params(layer_sizes, random.key(0))
+
+from jax.scipy.special import logsumexp
+
+def relu(x):
+    return jnp.maximum(0, x)
+
+def predict(params, image):
+    activations = image
+    for w, b in params[:-1]:
+        outputs = jnp.dot(w, activations) + b
+        activations = relu(outputs)
+
+    final_w, final_b = params[-1]
+    logits = jnp.dot(final_w, activations) + final_b
+    return logits - logsumexp(logits)
